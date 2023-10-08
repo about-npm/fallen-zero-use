92c1237bafa38e489430023e0942521e4b4d82a7
fallen-zero <fallen_zero@163.com>
2023-10-08 15:57:04 +0800

    添加请求方法

---
+++ src/useRequest/index.ts
@@ -0,0 +1,72 @@
+/*
+ * @Author       : fallen_zero
+ * @Date         : 2023-10-08 14:50:11
+ * @LastEditors  : fallen_zero
+ * @LastEditTime : 2023-10-08 15:42:46
+ * @FilePath     : /zero-use/src/useRequest/index.ts
+ * @FileName     :
+ */
+import { ref } from 'vue';
+
+/**
+ * 请求方法
+ * @param fn 请求函数
+ * @param params 请求参数
+ * @returns
+ */
+export const useRequest = <T extends (...args: any[]) => Promise<any>>(
+  fn: T,
+  ...params: Parameters<T>
+) => {
+  // 请求参数
+  let currParams = params;
+
+  // 请求数据
+  const data =
+    ref<T extends (...args: any[]) => Promise<infer R> ? R : unknown>();
+  // 错误信息
+  const error = ref();
+  // 是否正在请求
+  const loading = ref<boolean>(false);
+
+  /** 设置请求参数
+   * @param newParams 新的请求参数
+   */
+  const setParams = (...newParams: Parameters<T>) => {
+    currParams = newParams;
+  };
+
+  /** 开始请求 */
+  const start = async () => {
+    loading.value = true;
+    try {
+      const res = await fn(...currParams);
+      data.value = res;
+    } catch (err) {
+      error.value = err;
+    }
+
+    loading.value = false;
+  };
+
+  /** 重置请求 */
+  const reset = async () => {
+    currParams = params;
+    await start();
+  };
+
+  return {
+    /** 请求数据 */
+    data,
+    /** 请求错误信息 */
+    error,
+    /** 是否正在请求 */
+    loading,
+    /** 开始请求 */
+    start,
+    /** 重置请求 */
+    reset,
+    /** 设置请求参数 */
+    setParams,
+  };
+};
