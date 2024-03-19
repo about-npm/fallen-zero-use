8fc87c65774c1813a5ed44ade0bd27adb4b709c4
fallen-zero <fallen_zero@163.com>
2024-03-19 16:09:32 +0800

    修改Loader类

--- src/Loader/index.ts
+++ src/Loader/index.ts
@@ -2,7 +2,7 @@
  * @Author       : fallen_zero
  * @Date         : 2024-01-18 11:21:45
  * @LastEditors  : fallen_zero
- * @LastEditTime : 2024-03-19 15:48:09
+ * @LastEditTime : 2024-03-19 16:06:51
  * @FilePath     : /zero-use/src/Loader/index.ts
  * @FileName     :
  */
@@ -11,14 +11,20 @@ import { App, Component, createApp } from 'vue';
 export class Loader {
   private app: App<Element> | null = null;
 
+  /**
+   * @param comp 组件
+   * @param div 组件挂载的容器
+   * @param teleport 容器
+   */
   constructor(
     private comp: Component | (() => JSX.Element),
-    private div: Element = document.createElement('div')
+    private div: Element = document.createElement('div'),
+    private teleport: Element = document.body
   ) {}
 
   public show() {
     this.app = createApp(this.comp);
-    document.body.appendChild(this.div);
+    this.teleport.appendChild(this.div);
     this.app.mount(this.div);
   }
 
