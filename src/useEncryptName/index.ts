e0d83731f7a7fc59eac37fef212cc996b944d249
fallen-zero <fallen_zero@163.com>
2024-07-23 14:08:04 +0800

    添加脱敏函数

--- src/useEncryptName/index.ts
+++ src/useEncryptName/index.ts
@@ -2,10 +2,17 @@
  * @Author       : fallen_zero
  * @Date         : 2023-10-07 14:33:25
  * @LastEditors  : fallen_zero
- * @LastEditTime : 2023-12-07 10:15:43
- * @FilePath     : /zero-use/src/useEncryptName/index.ts
+ * @LastEditTime : 2024-07-23 14:04:26
+ * @FilePath     : /fallen-zero-use/src/useEncryptName/index.ts
  * @FileName     :
  */
+
+interface EncryptParams {
+  start?: number;
+  end?: number;
+  info?: string;
+  char?: string;
+}
 
 /**
  * 人名脱敏
@@ -24,12 +31,7 @@
  */
 export function encryptName(
   name: string,
-  {
-    start = 1,
-    end = 1,
-    info = '\u4e00-\u9fa5',
-    char = '*',
-  }: { start?: number; end?: number; info?: string; char?: string } = {}
+  { start = 1, end = 1, info = '\u4e00-\u9fa5', char = '*' }: EncryptParams = {}
 ): string {
   // 去除空白
   const trueName = name.replace(/\s/g, '');
@@ -44,4 +46,48 @@ export function encryptName(
     regExp,
     '$1' + char.repeat(Math.max(trueName.length - start - end, 1)) + '$2'
   );
+}
+
+/** 托名类型 */
+const EncryptType = {
+  name: {},
+  phone: {
+    start: 3,
+    end: 4,
+    info: 'd',
+    char: '*',
+  },
+  idCard: {
+    start: 3,
+    end: 4,
+    info: 'd',
+    char: '*',
+  },
+  bankCard: {
+    start: 6,
+    end: 4,
+    info: 'd',
+    char: '*',
+  },
+  email: {},
+};
+
+/**
+ * 根据类型进行脱敏
+ * @param {keyof typeof EncryptType} type 类型
+ * @param {string} str 待脱敏字符串
+ * @param {EncryptParams} [options] 配置参数
+ * @return {string} 脱敏后的字符串
+ */
+export function encrypt(
+  type: keyof typeof EncryptType,
+  str: string,
+  options: EncryptParams = {}
+): string {
+  if (type === 'email') {
+    // 邮箱脱敏
+    return str.replace(/^(.{1,2})@(.*)$/, '$1***@$2');
+  }
+  const typeOptions = (EncryptType[type] || {}) as EncryptParams;
+  return encryptName(str, { ...typeOptions, ...options });
 }
